6f62088b2e96b9b88a77ef84863cfd26d6c32b5e
MiguelApa2609 <miguel.goncalvessilva2009@gmail.com>
2025-04-10 08:46:11 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+//Js - JavaScript
+
+var nomes = ["Número 0 pertence ao nome: Luana","Número 1 pertence ao nome: Pedro","Número 2 pertence ao nome: Rafaela","Número 3 pertence ao nome Mariana","Número 4 pertence ao nome Kali","Número 5 pertence ao nome Kailany","SNúmero 6 pertence ao nome Samuel",
+"Número 7 pertence ao nome William", "Número 8 pertence ao nome Pietro", "Número 9 pertence ao nome Vinicius","Número 10 pertence ao nome Laura","Número 11 pertence ao nome Sarah","Número 12 pertence ao nome Pedro Henrique","Número 13 pertence ao nome Logan", "Número 14 pertence ao nome Miguel","Número 15 pertence ao nome João","Número 16 pertence ao nome Gustavo","Número 17 pertence ao nome Peter","Número 18 pertence ao nome Mario","Número 19 pertence ao nome Caio","Número 20 pertence ao nome Kaue","Número 21 pertence ao nome Heitor",
+"Número 22 pertence ao nome Will","Número 23 pertence ao nome Theo","Número 24 pertence ao nome Humberto","Número 25 pertence ao nome Hugo","Número 26 pertence ao nome Gabriel","Número 27 pertence ao nome Guilherme","Número 28 pertence ao nome Rafael","Número 29 pertence ao nome Vitor"]
+
+var nomeescolhido= parseInt(prompt("Digite um numero de 0 a 29"))
+
+
+document.write(nomes[nomeescolhido])
